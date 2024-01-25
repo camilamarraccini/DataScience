12393fc18a31dc734aa7402c4fe860a3ec236ed7
Camila Gabriel Marraccini <camilamarraccini1@gmail.com>
2024-01-25 12:59:02 -0300

    up validation

--- main.py
+++ main.py
@@ -49,4 +49,25 @@ scatter_matrix(dataset)
 plt.show()
 
 #Visualização OK, hora de trabalhar com algoritmos!
+
+
+#criando um conjunto de dados de VALIDAÇÃO
+# Split-out validation dataset
+array = dataset.values
+X = array[:,0:4]
+Y = array[:,4]
+validation_size = 0.20
+seed = 7
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
                       
+
+#VALIDAÇÃO CRUZADA (cross validation) para estimar a precisão:
+
+scoring = 'accuracy'
+
+#Regressão Logística (LR)
+#Análise Linear Discriminante (LDA)
+#K-vizinhos mais próximos (KNN).
+#Árvores de Classificação (Decision Tree) e Regressão (CART).
+#Gaussian Naive Bayes (NB).
+#Support Vector Machines (SVM).
