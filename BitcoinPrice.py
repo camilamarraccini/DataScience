dce8cd02acbc3bdc2c3e5f1f7cf1c70dc2c40559
Camila Gabriel Marraccini <camilamarraccini1@gmail.com>
2024-01-27 15:56:04 -0300

    trabalhando com colunas df

--- BitcoinPrice.py
+++ BitcoinPrice.py
@@ -7,8 +7,11 @@ current_directory = os.path.dirname(os.path.abspath(__file__))
 csv_path = os.path.join(current_directory, 'BTC-USD.csv')
 
 
-df = pd.read_csv(csv_path)
-print(df.head())
+df = pd.read_csv(csv_path, sep=";")
 print(df.columns)
+
+colunas = ['timestamp', 'name', 'close']
+df_formatado = df[colunas]
+print(df_formatado)
 
 
